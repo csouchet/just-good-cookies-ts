ab7fb0f4a5319f0e4eaf74401be90f08f65733da
Souchet Céline <celine.souchet@bonitasoft.com>
2022-08-02 17:15:10 +0200

    Add ESLint & Prettier configuration

---
+++ .eslintrc.js
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2022 Céline Souchet
+ *
+ * Licensed under the MIT License.
+ * See LICENSE file in the project root for full license information.
+ *
+ * This project is a fork of the original just-good-cookies project of Francesco Mugnai.
+ */
+
+module.exports = {
+  root: true,
+  plugins: ['notice'],
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  extends: [
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+  },
+  rules: {
+    'notice/notice': ['error', { templateFile: 'config/license-header.js', onNonMatchingHeader: 'replace' }],
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+  },
+  overrides: [
+    // typescript
+    {
+      files: ['*.ts'],
+      extends: [
+        'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+        'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+      ],
+      parserOptions: {
+        // This setting is required if you want to use rules which require type information
+        // https://github.com/typescript-eslint/typescript-eslint/tree/main/packages/parser#parseroptionsproject
+        project: ['./tsconfig.json', './tsconfig.test.json', './tsconfig.utils.json'],
+      },
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': [
+          'error',
+          {
+            allowExpressions: true,
+            allowTypedFunctionExpressions: true,
+          },
+        ],
+        '@typescript-eslint/explicit-member-accessibility': [
+          'error',
+          {
+            accessibility: 'no-public',
+          },
+        ],
+        '@typescript-eslint/consistent-type-exports': [
+          'error',
+          {
+            fixMixedExportsWithInlineTypeSpecifier: true,
+          },
+        ],
+        '@typescript-eslint/consistent-type-imports': ['error'],
+      },
+    },
+  ],
+};
